e33094e89006074bffc08cacb320a1c5054db18c
Nicola Hughes <nihughes@thoughtworks.com>
2017-02-15 16:58:02 +0000

    BAU: Fix broken workaround for Ububtu nodejs package
    Authors: @DataMinerUK @rhowe-gds

--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@ cd "$(dirname "$0")"
 export PATH=.gem-bin:node_modules/.bin:/usr/bin:$PATH
 
 # Horrid hack because node binary on Ubuntu is 'nodejs' not 'node'
-mkdir -p node_modules/.bin/node
+rm -rf node_modules/.bin/node
+mkdir -p node_modules/.bin
 if command -v nodejs; then
   ln -sf "$(command -v nodejs)" node_modules/.bin/node
-else
-  rm -f node_modules/.bin/node
 fi
 
 bundle install --path=.gems --binstubs=.gem-bin
