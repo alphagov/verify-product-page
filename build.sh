45f0245d313e7f6c2a354a4eea634940de2d512e
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2017-02-15 10:32:15 +0000

    BAU: Install bower and make it available to the build
    We need bower in order to install external dependencies from
    alphagov
    
    Pair: @rhowe-gds @DataMinerUK

--- build.sh
+++ build.sh
@@ -4,9 +4,17 @@ set -e
 
 cd "$(dirname "$0")"
 
-export PATH=.gem-bin:$PATH
+export PATH=.gem-bin:node_modules/.bin:$PATH
+
+# Horrid hack because node binary on Ubuntu is 'nodejs' not 'node'
+if command -v nodejs; then
+  ln -sf "$(command -v nodejs)" node_modules/.bin/node
+else
+  rm -f node_modules/.bin/node
+fi
 
 bundle install --path=.gems --binstubs=.gem-bin
+npm install bower
 bower install
 
 bundle exec middleman build
